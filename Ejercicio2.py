e2cfeb02ad2772ac5720da2630f10aaebf3ae64d
Dan1l0p <dalova2003@gmail.com>
2022-11-06 19:17:02 +0100

    ejr2

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,4 +1,16 @@
-def mdet(matriz):
-    return (matriz[0][0]*matriz[1][1]*matriz[2][2])+(matriz[1][0]*matriz[2][1]*matriz[0][2])-(matriz[0][2]*matriz[1][1]*matriz[2][0])-(matriz[0][1]*matriz[1][2]*matriz[2][0])
+lista = [3,2,3,5,3,4,9,1,7]
+for n in range (1, 4):
+    for v in range(1, 4):
+        matriz = int(input(f"Type a value [{n},{v}]"))
+        lista.append(matriz)
 
-print(mdet([[3,2,3],[5,3,4],[9,1,7]]))
+calc1 = (lista[0]*lista[4]*lista[8])
+calc2 = (lista[1]*lista[5]*lista[6])
+calc3 = (lista[2]*lista[3]*lista[7])
+calc4 = (lista[2]*lista[4]*lista[6])
+calc5 = (lista[0]*lista[5]*lista[7])
+calc6 = (lista[1]*lista[3]*lista[8])
+
+result = ((calc1 + calc2 + calc3)-(calc4 + calc5 + calc6))
+print(result)
+
--- Ejercicio2_1.py
+++ Ejercicio2_1.py
@@ -1,8 +1,8 @@
 import copy
 def mdet(matriz):
     suma = 0
-    if len(matriz) == 3 and len(matriz[0])==2:
-        suma = (matriz[0][0]*matriz[1][1]*matriz[2][2])+(matriz[1][0]*matriz[2][1]*matriz[0][2])-(matriz[0][2]*matriz[1][1]*matriz[2][0])-(matriz[0][1]*matriz[1][2]*matriz[2][0])
+    if len(matriz) == 3 and len(matriz[0])==3:
+        suma = ((matriz[0][0]*matriz[1][1]*matriz[2][2])+(matriz[1][0]*matriz[2][1]*matriz[0][2])+(matriz[0][1]*matriz[1][2]*matriz[2][0]))-((matriz[0][2]*matriz[1][1]*matriz[2][0])+(matriz[0][1]*matriz[1][0]*matriz[2][2])+(matriz[2][1]*matriz[1][2]*matriz[0][0]))
         return suma
 
     else:
